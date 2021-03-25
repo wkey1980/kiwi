d8b00646c7728a85d1b981323d4d52cac0d4ea22
will k <28814921+wkey1980@users.noreply.github.com>
2021-03-25 19:03:31 +0000

    Ypdated functions.php with

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@
 ?>
 
 <?php
+
 define( 'KIWI_THEME_DIR', get_template_directory() );
 define( 'KIWI_THEME_URI', get_template_directory_uri() );
 
@@ -51,47 +52,52 @@ if ( ! function_exists( 'kiwi_setup' ) ):
             )
         );
 
-                // Custom header, custom background, custom logo
-                $header_width  = 100;
-                $header_height  = 100;
-                add_theme_support(
-                    'custom-header',
-                    apply_filters( 'kiwi_custom_header_args',
-                    array(
-                        'default-color' => 'ffffff',   // Customize > Colors > Header Text Color
-                        'default-image' => '',
-                        'width'         => $header_width,
-                        'height'        => $header_height,
-                        'flex-height'   => true,
-                        'flex-width'    => true,
-                        )
-                    )
-                );
-                add_theme_support(
-                    'custom-background',
-                    apply_filters(
-                        'kiwi_custom_background_args',
-                        array(
-                            'default-color' => 'ffffff',   // Customize > Colors > Background Color
-                            'default-image' => '',
-                        )
-                    )
-                );
-                $logo_width = 50;
-                $logo_height = 50;
-                add_theme_support(
-                    'custom-logo',
-                    array(
-                        'width'       => $logo_width,
-                        'height'      => $logo_height,
-                        'flex-width'  => true,
-                        'flex-height' => true,
-                        'header-text' => array( 'title-tag', 'site-description' ),
-                    )
-                );
+        // Custom header, custom background, custom logo
+        $header_width  = 100;
+        $header_height  = 100;
+        add_theme_support(
+            'custom-header',
+            apply_filters( 'kiwi_custom_header_args',
+            array(
+                'default-color' => 'ffffff',   // Customize > Colors > Header Text Color
+                'default-image' => '',
+                'width'         => $header_width,
+                'height'        => $header_height,
+                'flex-height'   => true,
+                'flex-width'    => true,
+                )
+            )
+        );
+        add_theme_support(
+            'custom-background',
+            apply_filters(
+                'kiwi_custom_background_args',
+                array(
+                    'default-color' => 'ffffff',   // Customize > Colors > Background Color
+                    'default-image' => '',
+                )
+            )
+        );
+        $logo_width = 50;
+        $logo_height = 50;
+        add_theme_support(
+            'custom-logo',
+            array(
+                'width'       => $logo_width,
+                'height'      => $logo_height,
+                'flex-width'  => true,
+                'flex-height' => true,
+                'header-text' => array( 'title-tag', 'site-description' ),
+            )
+        );
 
-                add_theme_support( 'customize-selective-refresh-widgets' );
-
+        add_theme_support( 'customize-selective-refresh-widgets' );
     }
 endif;
 add_action( 'after_setup_theme', 'kiwi_setup' );
+
+function kiwi_content_width() {
+    $GLOBALS[ 'content-width' ] = apply_filters( 'kiwi_content_width', 1024 );
+}
+add_action( 'after_setup_theme', 'kiwi_content_width', 0 );
+
