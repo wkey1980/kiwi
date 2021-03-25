97b2c3b0e9c0ca9cef56d5980c6b4a65a3ac8fdc
will k <28814921+wkey1980@users.noreply.github.com>
2021-03-25 14:22:50 +0000

    added functions.php code

--- functions.php
+++ functions.php
@@ -7,3 +7,93 @@
  * @package kiwi
  */
 ?>
+
+<?php
+define( 'KIWI_THEME_DIR', get_template_directory() );
+define( 'KIWI_THEME_URI', get_template_directory_uri() );
+
+if ( ! function_exists( 'kiwi_setup' ) ): 
+    function kiwi_setup() {
+        load_theme_textdomain( 'kiwi', KIWI_THEME_DIR . '/Languages' );
+        add_theme_support( 'automatic-feed-links' );
+        add_theme_support( 'title-tag' );
+
+        add_theme_support( 'post-thumbnails' );
+        add_image_size( 'kiwi-large', 500, 400, true ); // width, height, crop
+
+        add_theme_support( 'customize-selective-refresh-widgets' );
+
+        add_theme_support(
+            'post-formats',
+            array(
+                'link',
+                'aside',
+                'gallery',
+                'image',
+                'quote',
+                'status',
+                'video',
+                'audio',
+                'chat',
+            )
+        );
+
+        add_theme_support(
+            'html5',
+            array(
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'style',
+                'script',
+                'navigation-widgets',
+            )
+        );
+
+                // Custom header, custom background, custom logo
+                // FIXME: Error with the $header_width & $header_height
+                $header_width  = 100;
+                $header_height  = 100;
+                add_theme_support(
+                    'custom-header',
+                    apply_filters( 'kiwi_custom_header_args',
+                    array(
+                        'default-color' => 'ffffff',   // Customize > Colors > Header Text Color
+                        'default-image' => '',
+                        'width'         => $header_width,
+                        'height'        => $header_height,
+                        'flex-height'   => true,
+                        'flex-width'    => true,
+                        )
+                    )
+                );
+                add_theme_support(
+                    'custom-background',
+                    apply_filters(
+                        'kiwi_custom_background_args',
+                        array(
+                            'default-color' => 'ffffff',   // Customize > Colors > Background Color
+                            'default-image' => '',
+                        )
+                    )
+                );
+                // FIXME: Error with the $logo_width & $logo_height
+                $logo_width = 50;
+                $logo_height = 50;
+                add_theme_support(
+                    'custom-logo',
+                    array(
+                        'width'       => $logo_width,
+                        'height'      => $logo_height,
+                        'flex-width'  => true,
+                        'flex-height' => true,
+                        'header-text' => array( 'title-tag', 'site-description' ),
+                    )
+                );
+
+                add_theme_support( 'customize-selective-refresh-widgets' );
+
+    }
+endif;
+add_action( 'after_setup_theme', 'kiwi_setup' );
