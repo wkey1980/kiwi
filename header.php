48a19273d6b48871b7e8a6ea8fe8859d83054569
will k <28814921+wkey1980@users.noreply.github.com>
2021-03-25 14:34:15 +0000

    addedd header.phpe code

--- header.php
+++ header.php
@@ -7,3 +7,26 @@
  * @package kiwi
  */
 ?>
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class((); ?>)>
+<?php wp_body_open(); ?>
+
+    <!-- Skip to content -->
+    <a class="skip-link screen-reader-text" href="#content">
+        <?php esc_html_e( 'Skip to Content', 'textDomain' ); ?>
+    </a>
+
+    <div id="page" class="site">
+        <header id="masthead" class="site-header">
+
+        </header><!-- end #masthead .site-header -->
+
+        <main id="content" class="site-content">
